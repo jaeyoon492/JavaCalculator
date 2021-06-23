b85d0ab19d71fda9e87aaa139e72ca66216f2db8
JaeYoonMyung <audwodbs492@gmail.com>
2021-06-24 00:49:41 +0900

    06/24 Calculator

--- src/domain/Calculator.java
+++ src/domain/Calculator.java
@@ -2,25 +2,27 @@ package domain;
 
 public class Calculator {
     InputPlace inputPlace = new InputPlace();
-    public int numberArea = inputPlace.numberArr.length;
+    int numberArea = 0;
 
     public int Calculation() {
         int result = 0;
         inputPlace.setOperations();
-        for (int i = 0; i < numberArea; i++) {
-            switch (inputPlace.operatorArr[i]) {
-                case "+":
-                    result = inputPlace.numberArr[i] + inputPlace.numberArr[i + 1];
-                    break;
-                case "-":
-                    result = inputPlace.numberArr[i] - inputPlace.numberArr[i + 1];
-                    break;
-                case "*":
-                    result = inputPlace.numberArr[i] * inputPlace.numberArr[i + 1];
-                    break;
-                case "/":
-                    result = inputPlace.numberArr[i] / inputPlace.numberArr[i + 1];
-                    break;
+        for (int i = 0; i < numberArea; i++) {  // 1 + 2 - 3 * 2
+            for (int j = 1; j < 4; j++){
+                switch (inputPlace.operatorArr[i]) {
+                    case "+":
+                        result = inputPlace.numberArr[i] + inputPlace.numberArr[i + j];
+                        break;
+                    case "-":
+                        result = inputPlace.numberArr[i] - inputPlace.numberArr[i + j];
+                        break;
+                    case "*":
+                        result = inputPlace.numberArr[i] * inputPlace.numberArr[i + j];
+                        break;
+                    case "/":
+                        result = inputPlace.numberArr[i] / inputPlace.numberArr[i + j];
+                        break;
+                }
             }
         }
         return result;
--- src/domain/InputPlace.java
+++ src/domain/InputPlace.java
@@ -7,7 +7,7 @@ public class InputPlace {
     String[] operatorArr;
     Scanner scanner = new Scanner(System.in);
 
-    public void setOperations() { // 1 * 2
+    public void setOperations() { // 1 + 2 - 3 * 2
         System.out.print("식을 입력해 주세요.(Ex : 1 + 2) : ");
         String[] Value = scanner.nextLine().split("\\s");
         numberArr = new int[Value.length];
